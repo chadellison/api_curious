1987b86e84bfbbc4adad773fd245da05d0f1d472
Chad Ellison <chad.ellison0123@gmail.com>
2016-05-24 16:34:05 -0600

    Add github service Add github user poro Add authorization key

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,22 +1,38 @@
 class GithubService
   attr_reader :user
+  attr_reader :connection
 
   def initialize(user)
     @user = user
+    @connection = Faraday.new(url: "https://api.github.com/users/")
+    @connection.headers["Authorization"] = "token #{user.oauth_token}"
   end
 
-  def followers
-    response = Faraday.get("https://api.github.com/users/#{user.login}/followers")
-    JSON.parse(response.body)
+  def get_followers
+    connection.get "#{user.login}/followers"
   end
 
-  def following
-    response = Faraday.get("https://api.github.com/users/#{user.login}/following")
-    JSON.parse(response.body)
+  def followers_hash
+    parse(get_followers)
   end
 
-  def starred
-    response = Faraday.get("https://api.github.com/users/#{user.login}/starred")
-    JSON.parse(response.body)
+  def get_following
+    connection.get "#{user.login}/following"
+  end
+
+  def following_hash
+    parse(get_following)
+  end
+
+  def get_starred
+    connection.get "#{user.login}/starred"
+  end
+
+  def starred_hash
+    parse(get_starred)
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
