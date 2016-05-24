44f6d6085d18ef070a3ab0af946b21d0baac5094
Chad Ellison <chad.ellison0123@gmail.com>
2016-05-23 18:26:33 -0600

    Allow user to login / sign up through github

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get "/auth/github", as: :github_login
   get "/auth/github/callback", to: "sessions#create"
-  get "/dashboard_path", to: "dashboard#show"
+  delete "/logout", to: "sessions#destroy"
 end
